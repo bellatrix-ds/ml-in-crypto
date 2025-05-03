20a22d8fd23cfabe82c5c54d0e3ac810d19c9c11
Bella <83490314+bellatrix-ds@users.noreply.github.com>
2025-05-03 23:22:20 +0300

    Update Wallet_Canvas_Dashboard.py

--- 04_Wallet_Canvas/Wallet_Canvas_Dashboard.py
+++ 04_Wallet_Canvas/Wallet_Canvas_Dashboard.py
@@ -13,24 +13,25 @@ wallet = st.selectbox("Select Wallet Address", df['wallet_address'])
 
 wallet_data = df[df['wallet_address'] == wallet].iloc[0]
 
+
+col1, col2 = st.columns(2)
 # part1: Basic Wallet Information
-st.header("🔹 Basic Wallet Information")
-st.write(f"**Wallet Address:** `{wallet_data['wallet_address']}`")
-st.metric("💰 ETH Balance", f"{wallet_data['eth']} ETH")
-st.metric("📦 Total Transactions (All Time)", wallet_data['tx_count'])
-st.metric("📆 Transactions in Last 3 Months", wallet_data['total_tx'])
-st.write(f"**🕒 First Transaction:** {wallet_data['first_tx']}")
-st.write(f"**🕒 Last Transaction:** {wallet_data['last_tx']}")
-
-# part 2: Transaction Behavio
-st.header("🔹 Transaction Behavior")
-st.metric("📈 Avg Value per Transaction (ETH)", f"{wallet_data['avg_tx_value']:.6f}")
-st.metric("💵 Avg Value per Transaction (USD)", f"${wallet_data['avg_value_usd']:.2f}")
-st.metric("🗓️ Avg Transactions per Day", f"{wallet_data['tx_per_day']:.2f}")
-st.metric("📅 Avg Transactions per Month", f"{wallet_data['tx_per_month']:.2f}")
-st.metric("⏳ Avg Time Between Transactions (days)", f"{wallet_data['avg_time_gap_days']}")
-
-
+with col1:
+    st.subheader("🟡 Basic Wallet Information")
+    st.write("**Wallet Address:**", wallet_address)
+    st.write("**💰 ETH Balance:**", eth_balance)
+    st.write("**📦 Total Transactions (All Time):**", f"{tx_count:,}")
+    st.write("**📆 Transactions in Last 3 Months:**", f"{total_tx:,}")
+    st.write("**🕒 First TX:**", first_tx)
+    st.write("**🕒 Last TX:**", last_tx)
+# part 2: Transaction Behavior
+with col2:
+    st.subheader("🟡 Transaction Behavior")
+    st.write("**📈 Avg TX Value (ETH):**", round(avg_tx_value, 4))
+    st.write("**💵 Avg TX Value (USD):**", f"${avg_value_usd:,.2f}")
+    st.write("**🗓️ Avg TX per Day:**", round(tx_per_day))
+    st.write("**📅 Avg TX per Month:**", round(tx_per_month))
+    st.write("**⏳ Avg Time Gap (days):**", avg_time_gap_days)
 
 # ـــ
 st.markdown("---")
