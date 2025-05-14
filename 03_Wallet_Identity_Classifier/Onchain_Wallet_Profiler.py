7a1432d38234b3007f1b6978aee7851c02db0960
Bella <83490314+bellatrix-ds@users.noreply.github.com>
2025-05-14 23:01:29 +0300

    Update Onchain_Wallet_Profiler.py

--- 03_Wallet_Identity_Classifier/Onchain_Wallet_Profiler.py
+++ 03_Wallet_Identity_Classifier/Onchain_Wallet_Profiler.py
@@ -7,6 +7,8 @@ import pandas as pd
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
+
 # --------------------------------------
 # Load main dataset
 df = pd.read_csv(
