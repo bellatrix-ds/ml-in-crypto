2fe040be59fe46abb05a10a2356f42a3450109c8
Bella <83490314+bellatrix-ds@users.noreply.github.com>
2025-05-16 19:10:47 +0300

    Update Onchain_Wallet_Profiler.py

--- 03_Wallet_Identity_Classifier/Onchain_Wallet_Profiler.py
+++ 03_Wallet_Identity_Classifier/Onchain_Wallet_Profiler.py
@@ -148,6 +148,7 @@ merged["MONTH_LABEL"] = merged["MONTH"].dt.strftime('%b-%Y')  # e.g. Jan-2024
 # --------------------------------------
 # 📈 Line chart
 st.markdown("---")
+# st.markdown()
 fig = go.Figure()
 
 fig.add_trace(go.Scatter(
@@ -167,7 +168,7 @@ fig.add_trace(go.Scatter(
 ))
 
 fig.update_layout(
-    title="f"### 👛 This Wallet’s Activity vs. **{selected_category}** Category"",
+    title="f"### 👛 This Wallet’s Activity vs. **{selected_category}** Category",
     xaxis_title="Month",
     yaxis_title="Transaction Count",
     legend_title="Legend",
